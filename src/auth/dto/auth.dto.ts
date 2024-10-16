501a1dddd5b3fdbf6263eb83583cbb988911b12a
André <casal.andre00@gmail.com>
2024-10-16 18:14:53 +0100

    removed the min length for password

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -17,8 +17,8 @@ export class CreateAccountDto {
   
     @IsNotEmpty()
     @IsString()
-    @MinLength(8, { message: 'Password must be at least 8 characters long.' })
-    /* @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+    /*@MinLength(8, { message: 'Password must be at least 8 characters long.' })
+     @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
       message: 'Password must include upper and lower case letters, numbers, and special characters.',
     }) */
     password: string;
